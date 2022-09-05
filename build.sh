cd1c08be94f3ccbd8cf1e2587e6548d444841efe
Michael Pu <michael.pu123@gmail.com>
2022-09-05 16:32:40 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ fi
 if [ -x "./build.sh" ]; then
   OUTPUT=`./build.sh "${CMD_PATH}"`
 else
-  go build "${CMD_PATH}"
+  go build "${CMD_PATH}" -buildvcs=false
   OUTPUT="${PROJECT_NAME}${EXT}"
 fi
 
